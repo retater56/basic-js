201ae2d07dbc5e480da2564ef4cb0bc59d514c04
retater56 <70779585+retater56@users.noreply.github.com>
2021-02-25 23:43:22 +0300

    feat: create a task solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  
+  if (!Array.isArray(arr)) {
+    throw Error
+  };
+
+  let resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    switch(arr[i]) {
+
+      case "--discard-next":
+        i++;
+        break;
+
+      case "--discard-prev":
+        (resultArr.length && arr[i - 2] != "--discard-next") ? resultArr.pop() : resultArr;
+        break;
+
+      case "--double-next":
+        (i < arr.length - 1) ? resultArr.push(arr[i + 1]) : resultArr;
+        break;
+
+      case "--double-prev":
+        (i > 0 && arr[i - 2] != "--discard-next") ? resultArr.push(arr[i - 1]) : resultArr;
+        break;
+
+      default:
+        resultArr.push(arr[i]);
+    }
+
+  }
+
+  return resultArr;
+
 };
