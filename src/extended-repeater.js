39b012c6446be2bcc7151174e332d4068c2a0135
retater56 <70779585+retater56@users.noreply.github.com>
2021-02-27 16:50:32 +0300

    feat: create a task solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (!options.separator) options.separator = '+';
+  if (!options.additionSeparator) options.additionSeparator = '|';
+  if (!options.repeatTimes) options.repeatTimes = 1;
+  if (!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+  if (options.addition === undefined) options.addition = '';
+
+  let result = '';
+  
+  for (let i = 0; i < +options.repeatTimes; i++) {
+    result += str;
+    for (let j = 0; j < +options.additionRepeatTimes; j++) {
+      result += options.addition;
+      if (j < +options.additionRepeatTimes - 1) {
+        result += options.additionSeparator;
+      }
+    }
+    if (i < +options.repeatTimes - 1) {
+      result += options.separator;
+    }
+  }
+
+  return result;
 };
   
